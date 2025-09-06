15003be77bf0ffb4225976e15e5e8cb200e67db4
Wasim Hyder <wasimhyder09@gmail.com>
2025-09-06 21:50:55 +0500

    Created custorm layouts

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+  return <div style={{ backgroundColor: '#575773', color: '#fff', textAlign: 'center', padding: '10px' }}>
+    <p>&copy; 2025 - Wasim Hyder</p>
+    </div>
+}
---
+++ src/app/components/header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+  return <div style={{ backgroundColor: '#a0a0c8', color: '#fff', textAlign: 'center', padding: '10px' }}>
+    <h1>React Routing Demo</h1>
+  </div>
+}
